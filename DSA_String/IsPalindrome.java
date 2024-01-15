32951754f1a9ae019f34f82e7a04ed7a17d94ea6
Shiv Raj Singh <shivraj.singh@aquera.com>
2024-01-15 12:24:18 +0530

    palindrome

---
+++ DSA_String/IsPalindrome.java
@@ -0,0 +1,29 @@
+package DSA_String;
+
+// this function will check whether given string is palindrome or not 
+class IsValidPalindrome {
+    public static boolean isValidPalindromeString(String str) {
+        if (str.length() <= 1) {
+            return true;
+        }
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) == str.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+public class IsPalindrome {
+    public static void main(String a[]) {
+        String x = "Mangal";
+        boolean res = IsPalindrome.isValidPalindromeString(x);
+        System.out.println(res);
+
+    }
+}
